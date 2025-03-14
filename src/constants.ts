81329791c3ea9cd7a8423ec0a1cc6f43ca680e7d
null <null@my-devbox>
2025-03-14 13:47:25 +0000

    github publish

--- src/constants.ts
+++ src/constants.ts
@@ -4,7 +4,7 @@
  */
 const constants = {
   APP_NAME: 'deno-box',
-  APP_VERSION: '0.1.1',
+  APP_VERSION: '0.1.2',
   APP_DESCRIPTION: 'A simple CLI app to quickly create a new Deno sandbox/playground project',
 };
 
